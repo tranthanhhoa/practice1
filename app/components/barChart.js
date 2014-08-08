16a6652dfb76a30daea0be50ae4d41f6d338d523
Tran Thanh Hoa <phamhoa.coder@gmail.com>
2014-08-08 18:07:41 +0700

    code in barChart.js

--- app/components/barChart.js
+++ app/components/barChart.js
@@ -29,8 +29,82 @@ define([
                         return d;
                     })])
                     .range([chartH, 0]);
+
                 var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
                 var yAxis = d3.svg.axis().scale(yScale).orient('left');
+
+                var barW = chartW / _data.length;
+
+                if (!svg) {
+                    svg = d3.select(this)
+                        .append("svg")
+                        .classed("chart", true);
+                    var container = svg.append('g').classed('conteiner-group', true);
+                    container.append('g').classed("chart-group", true);
+                    container.append('g').classed('x-axis axis', true);
+                    container.append('g').classed('y-axis axis', true);
+                }
+
+                svg.transition().duration(duration).attr({
+                    width: width,
+                    height: height
+                });
+                svg.select('.container-group')
+                    .attr({
+                        transform: "translate(" + margin.left + "," + margin.top + ")"
+                    });
+
+                svg.select('.x-axis.axis')
+                    .transition()
+                    .duration(duration)
+                    .ease(ease)
+                    .attr({
+                        transform: "translate(0," + chartH + ")"
+                    })
+                    .call(xAxis);
+
+                svg.select('.y-axis.axis')
+                    .transition()
+                    .duration(duration)
+                    .ease(ease)
+                    .call(yAxis);
+                var gapSize = xScale.rangeBand() / 100 * gap;
+                var barW = xScale.rangeBand() - gapSize;
+                var bars = svg.select('.chart-group')
+                    .selectAll('.bar')
+                    .data(_data);
+                bars.enter().append('rect').classed('bar', true)
+                    .attr({
+                        x: chartW,
+                        width: barW,
+                        y: function(d) {
+                            return yScale(d);
+                        },
+                        height: function(d) {
+                            return chartH - yScale(d);
+                        }
+                    })
+                bars.transition()
+                    .duration(duration)
+                    .ease(ease)
+                    .attr({
+                        width: barW,
+                        x: function(d, i) {
+                            return xScale(i) + gapSize / 2;
+                        },
+                        y: function(d) {
+                            return yScale(d);
+                        },
+                        height: function(d) {
+                            return chartH - yScale(d);
+                        }
+                    });
+
+                bars.exit().transition().style({
+                    opacity: 0
+                }).remove();
+
+                duration = 500;
             });
         }
 
